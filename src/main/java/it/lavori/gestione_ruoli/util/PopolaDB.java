2b1ef3f91c751b7aead8ea3b9ba2674f55ed4737
ValerioLP <valeriomezzoprete@gmail.com>
2022-05-18 15:53:35 +0200

    aggiunto popola utenti by csv

--- src/main/java/it/lavori/gestione_ruoli/util/PopolaDB.java
+++ src/main/java/it/lavori/gestione_ruoli/util/PopolaDB.java
@@ -1,14 +1,16 @@
 package it.lavori.gestione_ruoli.util;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import it.lavori.gestione_ruoli.model.Ruoli;
-import it.lavori.gestione_ruoli.model.Ruolo;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import it.lavori.gestione_ruoli.model.Utente;
 import it.lavori.gestione_ruoli.service.RuoloService;
 import it.lavori.gestione_ruoli.service.UtenteService;
@@ -25,10 +27,31 @@ public class PopolaDB implements CommandLineRunner {
 	RuoloService ruoloService;
 
 	@Override
-	public void run(String... args) {
-		Utente u = utenteService.getByNome("Nome1");
-		Ruolo r = ruoloService.getByNome(Ruoli.ADMIN);
-		u.addRuolo(r);
-		utenteService.update(u);
+	public void run(String... args) throws CsvValidationException {
+//		Utente u = utenteService.getByNome("Nome1");
+//		Ruolo r = ruoloService.getByNome(Ruoli.ADMIN);
+//		u.addRuolo(r);
+//		utenteService.update(u);
+		popolaUtentiByFile("utenti.csv");
+	}
+	
+	public void popolaUtentiByFile(String fileName) throws CsvValidationException {
+		try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8); CSVReader reader = new CSVReader(fr)) {
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+				for (String s2 : nextLine) {
+					String[] valori = s2.toLowerCase().split(";");
+					Utente u = new Utente();
+					u.setNome(valori[0]);
+					u.setCognome(valori[1]);
+					u.setEmail(valori[2]);
+					u.setPassword(valori[3]);
+					utenteService.insert(u);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Errore durante l'apertura del file");
+			e.printStackTrace();
+		}
 	}
 }
